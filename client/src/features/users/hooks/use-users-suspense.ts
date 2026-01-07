430ab10f5e80668c7dbdca0f6a0520a7ae91386c
okano.t <okano.t@world-wing.com>
2026-01-07 16:27:05 +0900

    refactor(users): ファイル命名規則を修正

--- client/src/features/users/hooks/use-users-suspense.ts
+++ client/src/features/users/hooks/use-users-suspense.ts
@@ -1,4 +1,4 @@
-import type { GetUsersQuery } from "server/src/schemas/users";
+import type { GetUsersQuery } from "server/src/schemas/users.schema";
 import { client, useHcSuspense } from "@/lib/api";
 
 export function useUsersSuspense({ page, limit }: GetUsersQuery) {
